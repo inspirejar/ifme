fbcf84c48757719967d8482eb222523e58020684
Julia Nguyen <tojulianguyen@gmail.com>
2018-04-08 19:43:32 -0700

    Developer steps for Viet translations

--- client/app/libs/i18n/I18nSetup.js
+++ client/app/libs/i18n/I18nSetup.js
@@ -14,6 +14,7 @@ import it from 'react-intl/locale-data/it';
 import nb from 'react-intl/locale-data/nb';
 import sv from 'react-intl/locale-data/sv';
 import nl from 'react-intl/locale-data/nl';
+import vn from 'react-intl/locale-data/vn';
 import br from 'react-intl/locale-data/br';
 
 const ptbr = first(br);
@@ -22,5 +23,5 @@ ptbr.locale = 'ptbr';
 // Initizalize all locales for react-intl.
 // eslint-disable-next-line import/prefer-default-export
 export const loadLocales = () => {
-  addLocaleData([...en, ...es, ...it, ...nb, ...sv, ...nl, ptbr]);
+  addLocaleData([...en, ...es, ...it, ...nb, ...sv, ...nl, ...vn, ptbr]);
 };
